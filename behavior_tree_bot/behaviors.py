1c98b9195d5343af84248be3e95ba43eb76dd3cf
Fence_Rider <enterprise.ekle@yahoo.com>
2020-07-05 13:26:55 -0700

    added a lot of calculation stuff imperial is not imperial cuz i started testing stuff

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -4,6 +4,8 @@ from planet_wars import issue_order
 import logging
 import functools 
 from heapq import heappop, heappush
+from math import ceil, sqrt
+from collections import namedtuple
 
 def attack_weakest_enemy_planet(state):
 
@@ -39,26 +41,121 @@ def spread_to_weakest_neutral_planet(state):
 
 
 
+def getCenter(planets):
+    #calculate center of mass
+    pos = functools.reduce(\
+    lambda a,b: [{"top":a[0]["top"] + (b.x*b.num_ships), "bottom":a[0]["bottom"] + b.num_ships},\
+                 {"top":a[1]["top"] + (b.y*b.num_ships), "bottom":a[1]["bottom"] + b.num_ships}],\
+                 planets, [{"top":0, "bottom":0},{"top":0, "bottom":0}])
+    pos[0] = pos[0]["top"]/pos[0]["bottom"]
+    pos[1] = pos[1]["top"]/pos[1]["bottom"]
+    
+    
+    #pos[0] = pos[0] / len(planets)
+    #pos[1] = pos[1] / len(planets) 
+    return pos[0], pos[1]
+def distance(pos1, pos2):
+    dx = pos1[0] - pos2[0]
+    dy = pos1[1] - pos2[1]
+    return int(ceil(sqrt(dx * dx + dy * dy)))
 
+"""
+    predicts a planets pop after all ships have been settled
+    also takes into account the growth rate
+    returns all planets
+"""
+def settled_predictPop(state):
+    #make both fleets and planets mutable
+    fleets = state.enemy_fleets() + state.my_fleets()
+    
+    fleets = [f._asdict() for f in fleets]
+
+    planets_list = state.not_my_planets() + state.my_planets()
+    
+    planets = {}
+    #key each planet by its id
+    for p in planets_list:
+        p = p._asdict()
+        planets[p['ID']] = p
+        #logging.info('\n' + str(planets[p['ID']]['x']))
+        
+    #planets = [p._asdict() for p in planets]
+    
+    while len(fleets) != 0:
+        for p in planets.items():
+            p = p[1] #PYTHON! plz give me debugger
+            if p['owner'] != 0:
+                p['num_ships'] = p['num_ships'] + p['growth_rate']
+        for f in fleets:
+            f['turns_remaining'] = f['turns_remaining'] - 1
+            if f['turns_remaining'] <= 0:
+                fleets.remove(f)
+                p = planets[f['destination_planet']]
+                if p['owner'] == f['owner']:
+                    p['num_ships'] += f['num_ships']
+                else:
+                    p['num_ships'] -= f['num_ships']
+                    if p['num_ships'] < 0:
+                        p['owner'] = ['f.owner']
+                        p['num_ships'] = abs(p['num_ships'])
+                        logging.info(p['num_ships'])
+    return planets
+        
+def toNametuple(dict_data):
+    return namedtuple(
+        "X", dict_data.keys()
+    )(*tuple(map(lambda x: x if not isinstance(x, dict) else toNametuple(x), dict_data.values())))
+
+def popAtArrival(planet, turns):
+    return planet.num_ships + (turns * planet.growth_rate)
 
 
 def imperial(state):
-
-    my_planets = state.my_planets().sort(key=lambda p : p.num_ships, reverse=True)
-    neutral_planets = state.neutral_planets().sort(key=lambda p : p.num_ships, reverse = True)
     
+    planets = settled_predictPop(state)
+    
+    #get the predicts population of planets, then sort them based on that
+    my_planets = []
+    #for p in planets.items():
+    #    logging.info('\n' + str(p[1]["ID"]))
+    #    if p[1]['owner'] == 1: #wtf is python!
+    #        my_planets.append(toNametuple(p[1]))
 
-    if len(my_planets) == 0 or len(neutral_planets) == 0:
+    #my_planets = [toNametuple(p[1]) for p in planets.items() if p[1]['owner'] == 1]
+    my_planets = state.my_planets()
+    my_planets.sort(key=lambda p : p.num_ships)
+    other_planets = [toNametuple(p[1]) for p in planets.items() if p[1]['owner'] != 1] # state.not_my_planets()#state.neutral_planets()
+    
+    my_center = getCenter(my_planets)
+    
+    #sorted distance
+    #neutral_planets.sort(key=lambda p : distance(getCenter(my_planets), (p.x,p.y) ))
+    #sorted lowest pop
+    #other_planets.sort(key=lambda p : p.num_ships )
+    #combined value
+    other_planets.sort(key=lambda p : popAtArrival(p, distance(getCenter(my_planets), (p.x,p.y))/3) * distance(getCenter(my_planets), (p.x,p.y) ))
+    
+    #logging.info('\n' + my_planets)
+
+    if len(my_planets) == 0 or len(other_planets) == 0:
         return False
     else:
-        for p in my_planets:
-            if len(neutral_planets) == 0:
+        for n in other_planets:
+            if len(other_planets) == 0:
                 break
-            current_n = neutral_planets.pop()
-            total = p.num_ships
-            if total - current_n.num_ships >= 10:
-                    issue_order(state, p.ID, current_n.ID, current_n.num_ships)
-            return True
+            
+            total = n.num_ships
+            while len(my_planets) > 0 and total > 0:
+                p = my_planets.pop()
+                if p.num_ships < 50:
+                    break
+                x = p.num_ships - 10 
+                
+                if x >0:
+                    total = total - x
+                    issue_order(state, p.ID, n.ID, p.num_ships-10)
+
+    return True
     
     pass
 
